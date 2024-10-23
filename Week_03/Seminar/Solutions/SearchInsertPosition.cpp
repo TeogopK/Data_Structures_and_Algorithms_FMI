b7e4b45607bfc0c47618854d71227b525cffa1d3
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-10-23 22:11:05 +0300

    Update SearchInsertPosition.cpp

--- Week_03/Seminar/Solutions/SearchInsertPosition.cpp
+++ Week_03/Seminar/Solutions/SearchInsertPosition.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-        
-        while(l <= r) {
-            int mid = l + (r - l) / 2;
-            // check the indexes
-            // - correct order is first occurence should be on even index
-            // - second on odd
-            // - if that is not correct - somewhere on the left side there is alone element
-            if(mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
-                if(mid & 1) { // correct placement
-                    l = mid + 1;
-                }
-                else {
-                    r = mid - 1;
-                }
-            }
-            else if(mid + 1 < nums.size() && nums[mid] == nums[mid + 1]) {
-                if(!(mid & 1)) { // correct placement
-                    l = mid + 1;
-                }
-                else {
-                    r = mid - 1;
-                }
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) {
+                right = mid - 1;
             }
             else {
-                return nums[mid];
+                left = mid + 1;
             }
         }
-        
-        return 0;
+
+        return left;
     }
 };
