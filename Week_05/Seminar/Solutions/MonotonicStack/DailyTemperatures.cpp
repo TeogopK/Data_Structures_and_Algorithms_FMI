c1cab633d41df52609fc6ad6c06ff9493eaf0399
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-11-06 22:06:13 +0200

    Update DailyTemperatures.cpp

--- Week_05/Seminar/Solutions/MonotonicStack/DailyTemperatures.cpp
+++ Week_05/Seminar/Solutions/MonotonicStack/DailyTemperatures.cpp
@@ -6,7 +6,9 @@ public:
         // contains indexes of elements in decreasing order
         stack<int> ms;
         for(size_t i = 0; i < temperatures.size(); i++) {
+            // pop all indexes of elements which are lower than the new temperature
             while(!ms.empty() && temperatures[ms.top()] < temperatures[i]) {
+                // for each popped index write in the result
                 result[ms.top()] = i - ms.top();
                 ms.pop();
             }
