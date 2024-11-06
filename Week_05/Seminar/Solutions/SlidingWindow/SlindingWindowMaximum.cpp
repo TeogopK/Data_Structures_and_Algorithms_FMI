2ac4b83117f4db6525f4da196457eaa4a236ec72
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-11-06 22:13:20 +0200

    Update SlindingWindowMaximum.cpp

--- Week_05/Seminar/Solutions/SlidingWindow/SlindingWindowMaximum.cpp
+++ Week_05/Seminar/Solutions/SlidingWindow/SlindingWindowMaximum.cpp
@@ -7,14 +7,17 @@ public:
         vector<int> result;
         
         for(int i = 0; i < nums.size(); i++) {
+            // keep the dq monotonic
             while(!dq.empty() && nums[i] >= nums[dq.back()]) {
                 dq.pop_back();
             }
             dq.push_back(i);
+            // pop the front if it is now outside the window!
             if(dq.front() <= i - k) {
                 dq.pop_front();
             }
-            
+
+            // start working on the result after you have passed atleast one window (k elements) to the dq
             if(i >= k - 1) {
                 result.push_back(nums[dq.front()]);
             }
