18d55841b1e9a9d191b9e96edc015b5d3427a28f
kostadinrusalov <rusalov@uni-sofia.bg>
2024-11-15 18:45:09 +0200

    add right side view

--- Week_06/Practicum/Binary Tree Right Side View.cpp
+++ Week_06/Practicum/Binary Tree Right Side View.cpp
@@ -1,3 +1,4 @@
+// solution with DFS
 class Solution {
     void dfs(TreeNode* node, int depth, vector<int>& view) {
         if (!node) {
@@ -15,6 +16,37 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> view;
         dfs(root, 0, view);
+        return view;
+    }
+};
+
+// solution with BFS
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+
+        std::vector<int> view;
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+
+        while (queue.size()) {
+            int levelSize = queue.size();
+            view.push_back(queue.front()->val);
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = queue.front();
+                if (node->right) {
+                    queue.push(node->right);
+                }
+                if (node->left) {
+                    queue.push(node->left);
+                }
+                queue.pop();
+            }
+        }
+
         return view;
     }
 };
