239c79da79a185b798601c1ea739c9881ecd2588
kostadinrusalov <rusalov@uni-sofia.bg>
2024-11-15 18:40:09 +0200

    add second solution

--- Week_06/Practicum/Path Sum III.cpp
+++ Week_06/Practicum/Path Sum III.cpp
@@ -1,16 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
- // memoization be like.....
+// memoization be like.....
 class Solution {
     void dfs(TreeNode* node, long targetSum, int& ans, int originalTargetSum, bool first){
         if(!node){
@@ -37,5 +25,32 @@ public:
         int ans = 0;
         dfs(root, targetSum, ans, targetSum, true);
         return ans;
+    }
+};
+
+class Solution {
+    int dfs(TreeNode* node, int targetSum, long prefixSum, std::map<long, int>& pathsWithPrefixSum) {
+        if (!node) {
+            return 0;
+        }
+
+        prefixSum += node->val;
+        int paths = 0;
+        if (pathsWithPrefixSum.count(prefixSum - targetSum)) {
+            paths += pathsWithPrefixSum[prefixSum - targetSum];
+        }
+
+        pathsWithPrefixSum[prefixSum]++;
+        paths += dfs(node->left, targetSum, prefixSum, pathsWithPrefixSum);
+        paths += dfs(node->right, targetSum, prefixSum, pathsWithPrefixSum); 
+        pathsWithPrefixSum[prefixSum]--;
+        return paths;
+    }
+
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        std::map<long, int> pathsWithPrefixSum;
+        pathsWithPrefixSum[0] = 1;
+        return dfs(root, targetSum, 0l, pathsWithPrefixSum);
     }
 };
