4641280ddcaa25850832cfdcbc45448eadc7bf32
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-11-27 20:43:33 +0200

    Create unordered_set.cpp

---
+++ Week_08/Seminar/Solutions/longest_consecutive_sequence/unordered_set.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> leftNumbers;
+        
+        for(auto n: nums) {
+            leftNumbers.insert(n);
+        }
+        
+        int maxLength = 0;
+        while(!leftNumbers.empty()) {
+            int innerLength = 1;
+            int low = *leftNumbers.begin();
+            int high = low;
+            // needed for O(1), otherwise you will traverse the same seq twice
+            leftNumbers.erase(low);
+            while(leftNumbers.find(low - 1) != leftNumbers.end()) {
+                low = low - 1;
+                innerLength += 1;
+                leftNumbers.erase(low);
+            }
+            while(leftNumbers.find(high + 1) != leftNumbers.end()) {
+                high = high + 1;
+                innerLength += 1;
+                leftNumbers.erase(high);
+            }
+            maxLength = max(maxLength, innerLength);
+        }
+        
+        return maxLength;
+    }
+};
