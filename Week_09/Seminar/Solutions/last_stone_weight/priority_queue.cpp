b629ee9038b47d0cff5877ee3113fcc4835fe1f8
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-04 21:57:16 +0200

    Update priority_queue.cpp

--- Week_09/Seminar/Solutions/last_stone_weight/priority_queue.cpp
+++ Week_09/Seminar/Solutions/last_stone_weight/priority_queue.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
-        for(auto s: stones) {
-            pq.push(s);
-        }
+        priority_queue<int> pq(stones.begin(), stones.end());
         
         while(pq.size() > 1) {
             int left = pq.top();
