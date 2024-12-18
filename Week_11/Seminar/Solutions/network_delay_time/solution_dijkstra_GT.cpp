e6d82131e9ebd7dfda3567e26c35d4db7eb89cef
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-18 14:13:05 +0200

    Update solution_dijkstra_GT.cpp

--- Week_11/Seminar/Solutions/network_delay_time/solution_dijkstra_GT.cpp
+++ Week_11/Seminar/Solutions/network_delay_time/solution_dijkstra_GT.cpp
@@ -13,27 +13,21 @@ struct Node {
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // create adj list
         unordered_map<int, vector<Edge>> graph;
         for(auto& edge: times) {
             graph[edge[0]].push_back({edge[1], edge[2]});
         }
-
-        // dijkstra
+        
         vector<int> distances(n + 1, INT_MAX);
-        unordered_set<int> visited;
         distances[k] = 0;
         priority_queue<Node> pq;
         pq.push({k, 0});
         while(!pq.empty()) {
             auto next = pq.top();
             pq.pop();
-            if(visited.count(next.index)) continue;
-            visited.insert(next.index);
+            if(distances[next.index] < next.distance) continue;
             
             for(auto& adj: graph[next.index]) {
-                if(visited.count(adj.to)) continue;
-                
                 int newDistance = next.distance + adj.weight;
                 if(newDistance < distances[adj.to]) {
                     distances[adj.to] = newDistance;
@@ -41,8 +35,7 @@ public:
                 }
             }
         }
-
-        // evaluate result
+        
         int maxElement = -1;
         for(size_t i = 1; i <= n; i++) {
             maxElement = std::max(maxElement, distances[i]);
