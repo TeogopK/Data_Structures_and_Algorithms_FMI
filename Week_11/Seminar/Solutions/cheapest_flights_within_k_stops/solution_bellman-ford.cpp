20b98ba2a9a657bf38cb60b7eb508e4f736dc4be
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-18 14:15:04 +0200

    Update solution_bellman-ford.cpp

--- Week_11/Seminar/Solutions/cheapest_flights_within_k_stops/solution_bellman-ford.cpp
+++ Week_11/Seminar/Solutions/cheapest_flights_within_k_stops/solution_bellman-ford.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<int> distances(n, INT_MAX);
-        
         distances[src] = 0;
         for(int i = 0; i <= k; i++) {
             // to make sure that every column is update only with data from last round + new edge
-            vector<int> temp(distances);
+            vector<int> nextRow(distances);
             for(auto& edge: flights) {
                 if(distances[edge[0]] != INT_MAX) {
-                    temp[edge[1]] = min(temp[edge[1]], distances[edge[0]] + edge[2]);
+                    nextRow[edge[1]] = min(nextRow[edge[1]], distances[edge[0]] + edge[2]);
                 }
             }
             
-            distances = std::move(temp);
+            distances = std::move(nextRow);
         }
         
         if(distances[dst] == INT_MAX) {
