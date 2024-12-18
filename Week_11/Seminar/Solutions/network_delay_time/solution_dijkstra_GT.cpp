4fd57e9ce601b7bdc86c079f2fd2695e3bbf7f2b
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-12-18 02:25:23 +0200

    Update solution_dijkstra_GT.cpp

--- Week_11/Seminar/Solutions/network_delay_time/solution_dijkstra_GT.cpp
+++ Week_11/Seminar/Solutions/network_delay_time/solution_dijkstra_GT.cpp
@@ -13,11 +13,13 @@ struct Node {
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // create adj list
         unordered_map<int, vector<Edge>> graph;
         for(auto& edge: times) {
             graph[edge[0]].push_back({edge[1], edge[2]});
         }
-        
+
+        // dijkstra
         vector<int> distances(n + 1, INT_MAX);
         unordered_set<int> visited;
         distances[k] = 0;
@@ -39,7 +41,8 @@ public:
                 }
             }
         }
-        
+
+        // evaluate result
         int maxElement = -1;
         for(size_t i = 1; i <= n; i++) {
             maxElement = std::max(maxElement, distances[i]);
