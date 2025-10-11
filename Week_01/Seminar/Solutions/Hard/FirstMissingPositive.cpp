49f073436810c8a87c6f694f2e56ea9f7787e4b4
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-11 14:46:34 +0300

    Update FirstMissingPositive.cpp

--- Week_01/Seminar/Solutions/Hard/FirstMissingPositive.cpp
+++ Week_01/Seminar/Solutions/Hard/FirstMissingPositive.cpp
@@ -1,24 +1,22 @@
-class Solution
-{
+class Solution {
 public:
-    int firstMissingPositive(vector<int> &nums)
-    {
-        for (size_t i = 0; i < nums.size(); i++)
-        {
-            while (nums[i] >= 1 && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i])
-            {
+    // O(n) time | O(1) space
+    int firstMissingPositive(vector<int>& nums) {
+        for(size_t i = 0; i < nums.size(); i++) {
+            // place every number at its coresponding index
+            // e.g. 3 should go to index 3 - 1 = 2
+            while(nums[i] >= 1 && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i]) {
                 std::swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        for (size_t i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] != i + 1)
-            {
+        // find the first the breaks the ordering we applied
+        for(size_t i = 0; i < nums.size(); i++) {
+            if(nums[i] != i + 1) {
                 return i + 1;
             }
         }
-
+        
         return nums.size() + 1;
     }
 };
