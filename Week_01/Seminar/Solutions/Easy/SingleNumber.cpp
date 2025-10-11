aaf63127415c70d32cd0c68e0170a9e52f9ba9d1
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-11 14:40:30 +0300

    Update SingleNumber.cpp

--- Week_01/Seminar/Solutions/Easy/SingleNumber.cpp
+++ Week_01/Seminar/Solutions/Easy/SingleNumber.cpp
@@ -2,12 +2,10 @@ class Solution
 {
 public:
     // O(n) time | O(1) space
-    int singleNumber(vector<int> &nums)
-    {
+    int singleNumber(vector<int> &nums) {
         int result = 0;
 
-        for (auto &num : nums)
-        {
+        for (auto &num : nums) {
             result ^= num;
         }
 
