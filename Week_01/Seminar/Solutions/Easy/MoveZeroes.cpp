122cdc456aa6aed915e9768bf5e407f17c3f71b7
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-11 14:36:47 +0300

    Update MoveZeroes.cpp

--- Week_01/Seminar/Solutions/Easy/MoveZeroes.cpp
+++ Week_01/Seminar/Solutions/Easy/MoveZeroes.cpp
@@ -1,15 +1,13 @@
 class Solution
 {
 public:
-    void moveZeroes(vector<int> &nums)
-    {
+    // O(n) time | O(1) space
+    void moveZeroes(vector<int> &nums) {
         int readIndex = 0;
         int writeIndex = 0;
 
-        while (readIndex < nums.size())
-        {
-            if (nums[readIndex] != 0)
-            {
+        while (readIndex < nums.size()) {
+            if (nums[readIndex] != 0) {
                 swap(nums[writeIndex], nums[readIndex]);
                 writeIndex++;
             }
