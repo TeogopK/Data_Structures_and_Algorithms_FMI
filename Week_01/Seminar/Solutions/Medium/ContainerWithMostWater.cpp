8839f9f7ad4b32fc04f33c96d1dc8ceb256f582f
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-11 14:54:45 +0300

    Update ContainerWithMostWater.cpp

--- Week_01/Seminar/Solutions/Medium/ContainerWithMostWater.cpp
+++ Week_01/Seminar/Solutions/Medium/ContainerWithMostWater.cpp
@@ -1,23 +1,19 @@
-class Solution
-{
+class Solution {
 public:
-    int maxArea(vector<int> &heights)
-    {
+    // O(n) time | O(1) space
+    int maxArea(vector<int>& heights) {
         int leftIdx = 0;
         int rightIdx = heights.size() - 1;
         int max = std::min(heights[leftIdx], heights[rightIdx]) * (rightIdx - leftIdx);
 
-        while (leftIdx < rightIdx)
-        {
+        while(leftIdx < rightIdx) {
             int currentMax = std::min(heights[leftIdx], heights[rightIdx]) * (rightIdx - leftIdx);
-
             max = std::max(max, currentMax);
-            if (heights[leftIdx] > heights[rightIdx])
-            {
+
+            if(heights[leftIdx] > heights[rightIdx]) {
                 rightIdx--;
             }
-            else
-            {
+            else {
                 leftIdx++;
             }
         }
