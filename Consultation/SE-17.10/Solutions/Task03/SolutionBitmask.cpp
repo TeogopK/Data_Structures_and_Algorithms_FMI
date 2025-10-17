e66f0f3ccef2bac6a0c217b65ecfcd77f7ef6566
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 00:25:19 +0300

    Fix upper bound calculation in subset generation

--- Consultation/SE-17.10/Solutions/Task03/SolutionBitmask.cpp
+++ Consultation/SE-17.10/Solutions/Task03/SolutionBitmask.cpp
@@ -5,17 +5,23 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> output;
-      
+
+        int upperBound = pow(2, n + 1);
         // generate bitmask, from 0..00 to 1..11
-        for (int i = pow(2, n); i < pow(2, n + 1); ++i) {
+        for (int i = pow(2, n); i < upperBound; i++) {
             string bitmask = bitset<32>(i).to_string().substr(32 - n);
+            
             // append subset corresponding to that bitmask
             vector<int> curr;
             for (int j = 0; j < n; ++j) {
-                if (bitmask.at(j) == '1') curr.push_back(nums[j]);
+                if (bitmask.at(j) == '1') {
+                    curr.push_back(nums[j]);
+                }
             }
+            
             output.push_back(curr);
         }
+        
         return output;
     }
 };
