07d2ab505c8f0a1fd77e5a173a9cabe4b8652ac4
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 10:40:33 +0300

    Create c++_solution_counting.cpp

---
+++ Week_02/Seminar/Solutions/FindTheDifference/c++_solution_counting.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        // vector<int> counter(256, 0);
+        int counter[256]{0};
+        for(auto& ch: s) {
+            counter[ch]++;
+        }
+        
+        for(size_t i = 0; i < t.size(); i++) {
+            counter[t[i]]--;
+            if(counter[t[i]] == -1) {
+                return t[i];
+            }
+        }
+        return -1;
+    }
+};
