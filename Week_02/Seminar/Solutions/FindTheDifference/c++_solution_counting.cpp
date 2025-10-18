a81254dc6ab1019b7e3894b7bbe1c769959d93fb
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 10:46:10 +0300

    Update c++_solution_counting.cpp

--- Week_02/Seminar/Solutions/FindTheDifference/c++_solution_counting.cpp
+++ Week_02/Seminar/Solutions/FindTheDifference/c++_solution_counting.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    // O(n) time | O(1) space
+    // O(1) space because the counter length is fixed to 26
     char findTheDifference(string s, string t) {
-        // vector<int> counter(256, 0);
-        int counter[256]{0};
+        // vector<int> counter(26, 0);
+        int counter[26]{0};
         for(auto& ch: s) {
-            counter[ch]++;
+            counter[ch - 'a']++;
         }
-        
-        for(size_t i = 0; i < t.size(); i++) {
-            counter[t[i]]--;
-            if(counter[t[i]] == -1) {
-                return t[i];
+        for(auto& ch: t) {
+            counter[ch - 'a']--;
+            if(counter[ch - 'a'] == -1) {
+                return ch;
             }
         }
+        
         return -1;
     }
 };
