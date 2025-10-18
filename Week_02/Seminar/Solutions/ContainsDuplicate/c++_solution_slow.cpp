f8b06bfa7167e610a0c687230617fedb09b4c1c2
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 10:46:44 +0300

    Update c++_solution_slow.cpp

--- Week_02/Seminar/Solutions/ContainsDuplicate/c++_solution_slow.cpp
+++ Week_02/Seminar/Solutions/ContainsDuplicate/c++_solution_slow.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // O(n ^ 2) time | O(1) space
     bool containsDuplicate(vector<int>& nums) {
         for (int i = 0; i < nums.size(); ++i) {
             for (int j = 0; j < i; ++j) {
