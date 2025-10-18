7cf7b18cdcbcc378e2bd6fd8fa977d677901d895
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 13:42:57 +0300

    Update c++_solution_slow.cpp

--- Week_02/Seminar/Solutions/SquaresOfASortedArray/c++_solution_slow.cpp
+++ Week_02/Seminar/Solutions/SquaresOfASortedArray/c++_solution_slow.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // O(n * log n) time | O(log n) space
     vector<int> sortedSquares(vector<int>& nums) {
         for(size_t i = 0; i < nums.size(); i++) {
             nums[i] = nums[i] * nums[i];
