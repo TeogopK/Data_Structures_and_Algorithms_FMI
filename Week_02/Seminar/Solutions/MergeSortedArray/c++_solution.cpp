c570ffff45699daf0db00e6c0f0ca64fae3fc9fe
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 13:46:10 +0300

    Create c++_solution.cpp

---
+++ Week_02/Seminar/Solutions/MergeSortedArray/c++_solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
+            }
+        }
+    }
+};
