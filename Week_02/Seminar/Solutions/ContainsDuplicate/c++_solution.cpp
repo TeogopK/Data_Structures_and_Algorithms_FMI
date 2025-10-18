8b6c111b30a0505c0099943b9fb3713d9da3cb48
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-18 10:47:26 +0300

    Update c++_solution.cpp

--- Week_02/Seminar/Solutions/ContainsDuplicate/c++_solution.cpp
+++ Week_02/Seminar/Solutions/ContainsDuplicate/c++_solution.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // complexity depends on the sorting algo behind the scenes. In c++ that is introsort
+    // O(n ^ log n) time | O(log n) space
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         for(size_t i = 1; i < nums.size(); i++) {
