739902d3a6f3d0f75eae134752d8dba3a68f038d
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-21 22:25:16 +0300

    Create c++_solution_quadratic.cpp

---
+++ Exams/Exam_01/Task2/c++_solution_quadratic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::vector<int> nums(n);
+	for (size_t i = 0; i < n; i++) {
+		std::cin >> nums[i];
+	}
+
+	std::vector<int> result(nums.size(), 0);
+	for (size_t i = 0; i < nums.size(); i++) {
+		size_t counter = 0;
+		for (size_t j = 0; j < nums.size(); j++) {
+			if (nums[j] > nums[i] && i != j) {
+				counter++;
+			}
+		}
+
+		result[i] = counter;
+	}
+
+
+	for (auto num : result) {
+		std::cout << num << " ";
+	}
+}
