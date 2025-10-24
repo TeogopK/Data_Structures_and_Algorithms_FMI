30a18cc9b43ec773b2fca436587f3013cb2ff8cc
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-24 17:17:59 +0300

    Update c++_solution_quadratic.cpp

--- Exams/Exam_01/Task2/c++_solution_quadratic.cpp
+++ Exams/Exam_01/Task2/c++_solution_quadratic.cpp
@@ -13,7 +13,7 @@ int main() {
 	for (size_t i = 0; i < nums.size(); i++) {
 		size_t counter = 0;
 		for (size_t j = 0; j < nums.size(); j++) {
-			if (nums[j] > nums[i] && i != j) {
+			if (nums[j] > nums[i]) {
 				counter++;
 			}
 		}
