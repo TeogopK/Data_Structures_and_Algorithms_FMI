3b1bbf95c22e9039fbddff4bf437dfda2d3868e4
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-31 12:26:17 +0200

    Create c++_solution_constant_space.cpp

---
+++ Week_04/Seminar/Solutions/OddEvenLinkedList/c++_solution_constant_space.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
