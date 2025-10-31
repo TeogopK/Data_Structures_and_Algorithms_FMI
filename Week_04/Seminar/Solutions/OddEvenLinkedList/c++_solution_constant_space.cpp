9cbac2a26beb772157a35cc7a386af794cfa95d2
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-10-31 12:27:27 +0200

    Update c++_solution_constant_space.cpp

--- Week_04/Seminar/Solutions/OddEvenLinkedList/c++_solution_constant_space.cpp
+++ Week_04/Seminar/Solutions/OddEvenLinkedList/c++_solution_constant_space.cpp
@@ -10,20 +10,29 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (!head) {
-            return nullptr;
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+            return head;
+        
+        int oddEvenIndicator = 1;
+        ListNode* lastOddNode = head;
+        ListNode* firstEvenNode = head->next;
+        ListNode* lastEvenNode = head->next;
+        ListNode* currentNode = head->next->next;
+        
+        while (currentNode != nullptr) {
+            if (oddEvenIndicator % 2 == 0) {
+                lastEvenNode->next = currentNode;
+                lastEvenNode = currentNode;
+            } else {
+                lastOddNode->next = currentNode;
+                lastOddNode = currentNode;
+            }
+            currentNode = currentNode->next;
+            oddEvenIndicator++;
         }
-
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-        while (even && even->next) {
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = odd->next;
-            even = even->next;
-        }
-        odd->next = evenHead;
+        
+        lastOddNode->next = firstEvenNode;
+        lastEvenNode->next = nullptr;
         return head;
     }
 };
