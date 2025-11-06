78e8244e6749830cf7d9a8d74de5caa28e3001de
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-11-07 00:44:18 +0200

    Update py_solution.py

--- Exams/Exam_02/Task2/py_solution.py
+++ Exams/Exam_02/Task2/py_solution.py
@@ -20,7 +20,7 @@ def positiveStalinSort(lst: Optional[Node]) -> Optional[Node]:
     current = head
     while current and current.next_node:
         # Свързваме текущия с по-следващия елемент, изтривайки по-малкия следващ
-        if current.data > current.next_node.data:
+        if current.next_node.data < 0 or current.data > current.next_node.data:
             current.next_node = current.next_node.next_node
             continue
         # В противен случай продължаваме напред
