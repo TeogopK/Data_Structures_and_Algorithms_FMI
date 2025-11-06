56d13ac1d8f7d9a259a727426fc7e7c12bfd8159
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-11-07 00:03:09 +0200

    Create c++_solution_decreasing_monostack.cpp

---
+++ Week_05/Seminar/Solutions/WelcomeToTheJungle/c++_solution_decreasing_monostack.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[100000000];
+int main() {
+    size_t count;
+    cin >> count;
+    for(size_t i = 0; i < count; i++) {
+        cin >> arr[i];
+    }
+
+    // decreasing monotonic stack which will hold the values of the elements
+    stack<int> s;
+    size_t maxSize = 0;
+    size_t maxStartingIndex = 0;
+    for(int i = count - 1; i >= 0; i--) {
+        int current = arr[i];
+        while(!s.empty() && current >= s.top()) {
+            s.pop();
+        }
+        s.push(current);
+        if(s.size() >= maxSize) {
+            maxSize = s.size();
+            maxStartingIndex = i;
+        }
+    }
+    
+    cout << maxStartingIndex;
+}
