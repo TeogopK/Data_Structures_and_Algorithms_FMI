cf035d72158756ba05e632e6ce57c46140a0fbef
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-11-07 00:35:28 +0200

    Create c++_solution_sentinel.cpp

---
+++ Exams/Exam_02/Task3/c++_solution_sentinel.cpp
@@ -0,0 +1,35 @@
+struct Node {
+    int value;
+    Node* next;
+    Node(int v) : value(v), next(nullptr) {}
+};
+
+Node* partition(Node* head, int x) {
+    if (!head) return head;
+
+    Node* lowerHead = new Node(0); // Sentinel node for elements less than x
+    Node* lowerTail = lowerHead;    // Tail of the lower list
+    Node* higherHead = new Node(0); // Sentinel node for elements greater or equal to x
+    Node* higherTail = higherHead;  // Tail of the higher list
+
+    Node* current = head;
+    while (current) {
+        if (current->value < x) {
+            lowerTail->next = current;
+            lowerTail = lowerTail->next;
+        } else {
+            higherTail->next = current;
+            higherTail = higherTail->next;
+        }
+        current = current->next;
+    }
+
+    lowerTail->next = higherHead->next; // Connect lower list to higher list
+    higherTail->next = nullptr;         // End of the new list
+
+    Node* newHead = lowerHead->next;
+    delete lowerHead; // Remove sentinel node
+    delete higherHead; // Remove sentinel node
+
+    return newHead;
+}
