ee7eec1085bd6c26b2392c92ced7db59b46b6da9
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-11-07 00:05:07 +0200

    Create c++_solution.cpp

---
+++ Week_05/Seminar/Solutions/RottingOranges/c++_solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited = grid;
+        //making queue in which we will fill rotten oranges
+        queue<pair<int, int>> q;
+        int countFreshOrange = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j] == 2) {
+                    q.push({i, j});
+                }
+                if (visited[i][j] == 1) {
+                    countFreshOrange++;
+                }
+            }
+        }
+        //q.empty() means there is no rotten orange in the grid and countFreshOrange = 0 means we will rotten the freshoranges in 0 mins
+        if (countFreshOrange == 0)
+            return 0;
+        if (q.empty())
+            return -1;
+        
+        int minutes = -1;
+        // we will cover four directions i.e. up, down, left, right
+        vector<pair<int, int>> dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (auto [dx, dy] : dirs) {
+                    int i = x + dx;
+                    int j = y + dy;
+                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
+                        visited[i][j] = 2;
+                        countFreshOrange--;
+                        q.push({i, j});
+                    }
+                }
+            }
+            minutes++;
+        }
+        
+        if (countFreshOrange == 0)
+            return minutes;
+        return -1;
+    }
+};
