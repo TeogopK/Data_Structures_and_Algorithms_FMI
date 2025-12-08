25cba7fb0d2c5521c37069b6288c68a9eb66771e
presiyantodorov-fourth <103115205+presiyantodorov-fourth@users.noreply.github.com>
2025-12-08 22:19:32 +0200

    Add files via upload

---
+++ Week_09/Seminar/Solution/LongestIncreasingSubsequence/py_solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLIS(self, nums) -> int:
+        dp = [1 for _ in nums]
+        for i in range(len(nums)):
+            # for each i we will give an answer what is the length of the LIS that ends with 
+            # nums[i]. The base case is 1, each number can make a subsequence of length 1.
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    # If a number before nums[i] is lower than nums[i], then we can
+                    # make a longer increasing subsequence by 1 over the one we can make
+                    # ending with nums[j]
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+
+        return max(dp)
+
+            
+            
+        
