225943f987efb976b131c098e90e816f421346b0
asen-krasimirov <personalgrowth57@gmail.com>
2025-12-08 18:34:19 +0200

    Remove useless comments from Week_09 problem

--- Week_09/Practicum/python/91_Decode_Ways.py
+++ Week_09/Practicum/python/91_Decode_Ways.py
@@ -5,12 +5,12 @@ class Solution:
 
         n = len(s)
         dp = [0] * (n + 1)
-        dp[0] = 1  # empty string
-        dp[1] = 1  # first character valid
+        dp[0] = 1
+        dp[1] = 1
 
         for i in range(2, n + 1):
-            one = int(s[i - 1:i])     # last 1 digit
-            two = int(s[i - 2:i])     # last 2 digits
+            one = int(s[i - 1:i])
+            two = int(s[i - 2:i])
 
             if 1 <= one <= 9:
                 dp[i] += dp[i - 1]
