58c2274303a3e0aebb18c6f73fe28604fe33f8b0
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-12-20 21:37:37 +0200

    Add comment for maxHeap purpose in Task2
    Added a comment to clarify the purpose of maxHeap.

---
+++ Exams/Seminar/Exam_05/Task2/c++_solution_short_code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int k;
+    std::cin >> k;
+
+    int num;
+    // holds the k-min elements
+    std::priority_queue<int> maxHeap;
+
+    while (std::cin >> num && num != -1) {
+        if (num == 0) {
+            std::cout << ((maxHeap.size() < k) ?  -1 : maxHeap.top()) << std::endl;
+            continue;
+        }
+
+        // if it is greater it will be popped in the if
+        maxHeap.push(num);
+        if (maxHeap.size() > k) {
+            maxHeap.pop();
+        }
+    }
+}
